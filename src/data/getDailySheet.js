0b4a4cad3ab6256dd037f1d0ac552d578860574a
Wevesson Madson <wevesson.madson@hotmail.com>
2024-04-18 15:15:34 -0300

    criado o diretorio e arquivos de data, para buscar informações e salvar no localstorage.

---
+++ src/data/getDailySheet.js
@@ -0,0 +1,30 @@
+export default function getDailySheet() {
+    const dataSheet = localStorage.getItem('dailySheet');
+  
+    let dailySheet = {};
+    
+    if (!dataSheet) {
+        let date = new Date();
+        let day = date.getDate();
+  
+        if (date.getHours() > 19) {
+            day += 1;
+        }
+        
+        dailySheet = {
+          date: `${day}/${date.getMonth() > 10 ? date.getMonth() : "0" + date.getMonth()}/${date.getFullYear()}`,
+          opened: true,
+          createdAt: date,
+          focus: '',
+          priorities: [[{ done: false, text: '' }, { done: false, text: '' }, { done: false, text: '' }, { done: false, text: '' }, { done: false, text: '' }]],
+          ignories: ['', '', ''],
+          dayNote: '',
+          todayLearnings: ['', '', '', '', ''],
+          todayGratefull: ['', '', '']
+        }
+    } else {
+      dailySheet = JSON.parse(dataSheet);
+    }
+  
+    return dailySheet;
+}
